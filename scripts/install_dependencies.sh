9f0d5c32d50d9ae7d0eda242a97678412f63912b
Jaikar Amalraj <jaikar.amalraj@bjss.com>
2022-03-08 23:46:34 +0000

    updated version

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 -m venv venv
 source venv/bin/activate
-cd /home/ec2-user/JaFlaskWebDbAppSrvr
+cd /home/ec2-user/JaFlaskWebDbApp
 sudo pip install flask
 sudo pip install -r requirements.txt
 FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=5000
