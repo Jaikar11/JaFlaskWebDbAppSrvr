92a7e1462745370d2e465a0b1f7612a7b6493725
Jaikar Amalraj <jaikar.amalraj@bjss.com>
2022-03-18 01:17:40 +0000

    updated verion

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,5 +3,5 @@ python3 -m venv venv
 source venv/bin/activate
 cd /home/ec2-user/
 cd /home/ec2-user/JaFlaskWebDbAppSrvr
-pip3 install flask --user
+sudo pip3 install flask
 sudo pip3 install -r requirements.txt
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,4 +5,5 @@ cd /home/ec2-user/JaFlaskWebDbAppSrvr
 source scripts/register_with_elb.sh
 touch log.txt
 chmod 777 log.txt
-FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=5000 >>log.txt 2>&1 &
+FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=5000
+#FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=5000 >>log.txt 2>&1 &
