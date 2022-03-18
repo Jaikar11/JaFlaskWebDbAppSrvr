b62505ff0dcc58ca2f2a57839b799af43ed254e1
Jaikar Amalraj <jaikar.amalraj@bjss.com>
2022-03-18 11:39:48 +0000

    Updated Version

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,6 +2,7 @@
 python3 -m venv venv
 source venv/bin/activate
 cd /home/ec2-user/
+aws s3 cp s3://ja-day12-config/rds_endpoint.txt
 cd /home/ec2-user/JaFlaskWebDbAppSrvr
 sudo pip3 install flask
 sudo pip3 install -r requirements.txt
