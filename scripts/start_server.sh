d4acbe1edf75066d4fc218d460b2a874527c1487
Jaikar Amalraj <jaikar.amalraj@bjss.com>
2022-03-20 00:36:59 +0000

    updated version

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,6 +4,6 @@ source venv/bin/activate
 cd /home/ec2-user/JaFlaskWebDbAppSrvr
 touch log.txt
 chmod 777 log.txt
-source scripts/register_with_elb.sh
 #FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=5000
 FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=5000 >>log.txt 2>&1 &
+source scripts/register_with_elb.sh
