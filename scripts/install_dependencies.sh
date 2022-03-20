d05690eb29ba73e8ca8865b26714b9dab387a055
Jaikar Amalraj <jaikar.amalraj@bjss.com>
2022-03-20 00:10:13 +0000

    updated version

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,9 +3,9 @@ python3 -m venv venv
 source venv/bin/activate
 cd /home/ec2-user/
 sudo aws s3 cp s3://ja-day12-config/rds_endpoint.txt rds_endpoint.txt
+cd /home/ec2-user/JaFlaskWebDbAppSrvr
 sudo touch ./templates/ipaddress.txt
 chmod 777 ./templates/ipaddress.txt
-sudo curl checkip.amazonaws.com >>./templates/ipaddress.txt
-cd /home/ec2-user/JaFlaskWebDbAppSrvr
+sudo curl checkip.amazonaws.com >> ./templates/ipaddress.txt
 sudo pip3 install flask
 sudo pip3 install -r requirements.txt
