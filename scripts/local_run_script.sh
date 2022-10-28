993b8a4300c390bb39b763c0f4cd82c16af94daa
Jaikar Amalraj <jaikar.amalraj@bjss.com>
2022-10-28 12:00:25 +0100

    Updated with code with local script

--- scripts/local_run_script.sh
+++ scripts/local_run_script.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+cd ~/repos
+python3 -m venv venv
 source venv/bin/activate
-cd ~/JaFlaskWebDbAppSrvr
-touch log.txt
-chmod 777 log.txt
-sudo touch ./ipaddress.txt
-sudo chmod 777 ./ipaddress.txt
+cd JaFlaskWebDbAppSrvr/
+FILE1="log.txt"
+FILE2="ipaddress.txt"
+if [ ! -f "$FILE1"]; then
+    touch "$FILE1"
+    echo "$FILE1 created."
+fi
+if [ ! -f "$FILE2"]; then
+    touch "$FILE2"
+    echo "$FILE2 created."
+    chmod 777 "$FILE2"
+fi
+chmod 777 "$FILE1"
+chmod 777 "$FILE2"
 hostname -I | awk '{print $1}' > ./ipaddress.txt
-sudo pip3 install flask
 sudo pip3 install -r requirements.txt
 FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=5000
 #FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=5000 >>log.txt 2>&1 &
-
