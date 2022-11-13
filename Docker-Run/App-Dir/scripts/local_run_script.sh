553390cdddee094553546846a9429b310b11a6eb
Jaikar Amalraj <jaikar.amalraj@bjss.com>
2022-11-13 22:22:40 +0000

    Included Docker Run

---
+++ Docker-Run/App-Dir/scripts/local_run_script.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#set -x
+
+cd ~/Ja-Repos/JaFlaskWebDbAppSrvr/Docker-Run/App-Dir
+if [[ "$(python3 -V)" =~ "Python 3" ]];then
+    echo "Python 3 is installed"
+else
+    echo "ERROR: Python 3 is available"
+    exit 1 # terminate as Python3 is not available
+fi
+
+if [[ "$(pip3 -V)" =~ "not found" ]];then
+    sudo apt install python3-pip -y
+    if [ "$(pip3 -V)" =~ "not found" ];then
+        echo "ERROR: Python 3 pip is not installed / available"
+        exit 1 # terminate as pip3 is not available
+    else
+        echo "Python pip3 is installed now"
+    fi
+else
+    echo "Python pip3 is available"
+fi
+
+if [[ "$(virtualenv --version)" =~ "not found" ]];then
+    sudo apt install python3-virtualenv -y
+    if [ "$(pip3 -V)" =~ "not found" ];then
+        echo "ERROR: virtualenv pip is not installed / available"
+        exit 1 # terminate as virtualenv is not available
+    else
+        echo "Python virtualenv is installed now"
+    fi
+else
+    echo "virtualenv is available"
+fi
+
+python3 -m venv venv
+source venv/bin/activate
+FILE1="log.txt"
+FILE2="ipaddress.txt"
+if [ ! -f "$FILE1" ]; then
+    touch "$FILE1"
+    echo "$FILE1 created."
+fi
+if [ ! -f "$FILE2" ]; then
+    touch "$FILE2"
+    echo "$FILE2 created."
+fi
+chmod 777 "$FILE1"
+chmod 777 "$FILE2"
+hostname -I | awk '{print $1}' > ./ipaddress.txt
+echo "localhost" > ./rds_endpoint.txt
+sudo pip3 install -r requirements.txt
+sudo FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=80
+#FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=5000 >>log.txt 2>&1 &
---
+++ Docker-Run/README.md
@@ -0,0 +1,3 @@
+This is a Flask WebApplication connecting to an existing Mysql DB
+ipaddress.txt and rds_endpoint.txt are updated by parameters by local_run_script.sh
+sqldbparms.txt has to be updated manually. 
