e87fe1f204ce08048d96fca64a29347168e9b0af
Ja <j.a@raspsrvr.com>
2022-11-15 11:55:12 +0000

    updated code

--- Docker-Run/App-Dir/scripts/local_run_script.sh
+++ Docker-Run/App-Dir/scripts/local_run_script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #set -x
 
-cd ~/Ja-Repos/JaFlaskWebDbAppSrvr/Docker-Run/App-Dir
+#cd ~/Ja-Repos/JaFlaskWebDbAppSrvr/Docker-Run/App-Dir
 if [[ "$(python3 -V)" =~ "Python 3" ]];then
     echo "Python 3 is installed"
 else
     echo "ERROR: Python 3 is available"
-    exit 1 # terminate as Python3 is not available
+#    exit 1 # terminate as Python3 is not available
 fi
 
 if [[ "$(pip3 -V)" =~ "not found" ]];then
     sudo apt install python3-pip -y
     if [ "$(pip3 -V)" =~ "not found" ];then
         echo "ERROR: Python 3 pip is not installed / available"
-        exit 1 # terminate as pip3 is not available
+#        exit 1 # terminate as pip3 is not available
     else
         echo "Python pip3 is installed now"
     fi
@@ -21,19 +21,17 @@ else
     echo "Python pip3 is available"
 fi
 
-if [[ "$(virtualenv --version)" =~ "not found" ]];then
-    sudo apt install python3-virtualenv -y
+if [[ "$(venv --version)" =~ "not found" ]];then
     if [ "$(pip3 -V)" =~ "not found" ];then
         echo "ERROR: virtualenv pip is not installed / available"
-        exit 1 # terminate as virtualenv is not available
+ #       exit 1 # terminate as virtualenv is not available
     else
-        echo "Python virtualenv is installed now"
+        echo "Python env is installed now"
     fi
 else
-    echo "virtualenv is available"
+    echo "venv is available"
 fi
-
-python3 -m venv venv
+python3 -m virtualenv venv
 source venv/bin/activate
 FILE1="log.txt"
 FILE2="ipaddress.txt"
@@ -47,8 +45,9 @@ if [ ! -f "$FILE2" ]; then
 fi
 chmod 777 "$FILE1"
 chmod 777 "$FILE2"
-hostname -I | awk '{print $1}' > ./ipaddress.txt
-echo "localhost" > ./rds_endpoint.txt
-sudo pip3 install -r requirements.txt
-sudo FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=80
+#hostname -I | awk '{print $1}' > ./ipaddress.txt
+echo "docker"> ./ipaddress.txt
+echo "192.168.0.47" > ./rds_endpoint.txt
+pip3 install -r requirements.txt
+FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=5000
 #FLASK_APP=App.app.py flask run --host='0.0.0.0' --port=5000 >>log.txt 2>&1 &
