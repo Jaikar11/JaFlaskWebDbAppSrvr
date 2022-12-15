c97ceb1b1e212491e7ffaa50561c5edc00a3dbb2
Jaikar Amalraj <jaikar.amalraj@bjss.com>
2022-12-15 14:48:19 +0000

    Updated version

--- Docker-Run/App-Dir/App/App.py
+++ Docker-Run/App-Dir/App/App.py
@@ -196,6 +196,22 @@ def data():
         print("New Row Inserted")
         return render_template('data.html',form_data = form_data)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
 @app.route('/logout')
 def logout():
     # Remove session data, this will log the user out
